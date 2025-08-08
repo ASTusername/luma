a6a0245ce4ee417bc2db7dee23b2989a91516b45
Alex <2231790@gmail.com>
2025-08-08 22:52:21 +0300

    добавил условие запуска браузера через selenoid или локально remoteDriver=selenoid

--- src/test/java/runner/BaseTest.java
+++ src/test/java/runner/BaseTest.java
@@ -20,14 +20,18 @@ public abstract class BaseTest {
         Configuration.baseUrl = "https://magento.softwaretestingboard.com/";
         Configuration.pageLoadStrategy = "eager";
 
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        // Проверяем системное свойство
+        String remoteDriver = System.getProperty("remoteDriver");
+        if ("selenoid".equalsIgnoreCase(remoteDriver)) {
+            Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
-                "enableVNC", true,
-                "enableVideo", true
-        ));
-        Configuration.browserCapabilities = capabilities;
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                    "enableVNC", true,
+                    "enableVideo", true
+            ));
+            Configuration.browserCapabilities = capabilities;
+        }
 
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
